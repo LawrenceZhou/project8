f4708199933b066c58c8bc5d76bf22ccaec2cacf
LawrenceZhou <hmskyijun@gmail.com>
2017-03-14 22:31:29 -0700

    0.89

--- components/user-detail/user-detailController.js
+++ components/user-detail/user-detailController.js
@@ -1,7 +1,7 @@
 'use strict';
 
-cs142App.controller('UserDetailController', ['$scope', '$routeParams', '$resource',
-  function ($scope, $routeParams, $resource) {
+cs142App.controller('UserDetailController', ['$scope', '$routeParams', '$resource', '$location',
+  function ($scope, $routeParams, $resource, $location) {
     /*
      * Since the route is specified as '/users/:userId' in $routeProvider config the
      * $routeParams  should have the userId property set with the path from the URL.
